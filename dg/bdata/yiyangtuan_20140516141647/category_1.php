a8829ebf985690237996ff55389fb6c2e346cf70
lujiefeng <lujfsd@163.com>
2014-07-28 13:13:14 +0800

    修改了创建表group_bond的表结构
    去掉了password项

--- dg/bdata/yiyangtuan_20140516141647/category_1.php
+++ dg/bdata/yiyangtuan_20140516141647/category_1.php
@@ -23,16 +23,16 @@ E_C("CREATE TABLE `category` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `UNQ_zne` (`zone`,`name`,`ename`)
 ) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8");
-E_D("replace into `category` values('1','city','','临沂','linyi','L','1','Y',NULL,'0');");
-E_D("replace into `category` values('2','city','热门','北京','beijing','B','2','Y',NULL,'0');");
-E_D("replace into `category` values('3','group',NULL,'生活类','shenghuolei','S','1','Y',NULL,'0');");
-E_D("replace into `category` values('4','group',NULL,'商品类','shangpinlei','S','2','Y',NULL,'0');");
-E_D("replace into `category` values('5','group',NULL,'理发','lifa','L','1','Y',NULL,'3');");
-E_D("replace into `category` values('6','group',NULL,'化妆品','huazhuangpin','H','1','Y',NULL,'4');");
-E_D("replace into `category` values('7','grade','初级会员111','初级会员','chujihuiyuan','C','1','Y',NULL,'0');");
-E_D("replace into `category` values('8','partner','11111','vip客户','vipkehu','V','1','Y',NULL,'0');");
-E_D("replace into `category` values('9','express','ppp','pp','pp','P','1','Y','5','0');");
-E_D("replace into `category` values('10','grade','y','中级会员','zhongjiuiyuan','Z','1','Y',NULL,'0');");
+//E_D("replace into `category` values('1','city','','临沂','linyi','L','1','Y',NULL,'0');");
+//E_D("replace into `category` values('2','city','热门','北京','beijing','B','2','Y',NULL,'0');");
+//E_D("replace into `category` values('3','group',NULL,'生活类','shenghuolei','S','1','Y',NULL,'0');");
+//E_D("replace into `category` values('4','group',NULL,'商品类','shangpinlei','S','2','Y',NULL,'0');");
+//E_D("replace into `category` values('5','group',NULL,'理发','lifa','L','1','Y',NULL,'3');");
+//E_D("replace into `category` values('6','group',NULL,'化妆品','huazhuangpin','H','1','Y',NULL,'4');");
+//E_D("replace into `category` values('7','grade','初级会员111','初级会员','chujihuiyuan','C','1','Y',NULL,'0');");
+//E_D("replace into `category` values('8','partner','11111','vip客户','vipkehu','V','1','Y',NULL,'0');");
+//E_D("replace into `category` values('9','express','ppp','pp','pp','P','1','Y','5','0');");
+//E_D("replace into `category` values('10','grade','y','中级会员','zhongjiuiyuan','Z','1','Y',NULL,'0');");
 
 require("../../inc/footer.php");
 ?>
